3af005fdcbe8ddada83b2b75fbdeaae95cd3ca5f
aissa-dev1 <aissadev1@gmail.com>
2025-05-23 09:40:25 +0100

    Create QuoteOfTheDaySection and QuoteBlock and quotes data

---
+++ src/components/QuoteBlock/QuoteBlock.tsx
@@ -0,0 +1,22 @@
+import styles from "./QuoteBlock.module.scss";
+
+interface Quote {
+  id: string;
+  quote: string;
+  author: string;
+}
+
+interface QuoteBlockProps {
+  quote: Quote;
+}
+
+const QuoteBlock: React.FC<QuoteBlockProps> = ({ quote }) => {
+  return (
+    <blockquote className={styles.quoteBlock}>
+      <p className={styles.quoteText}>{quote.quote}</p>
+      <footer className={styles.author}>— {quote.author}</footer>
+    </blockquote>
+  );
+};
+
+export { QuoteBlock };
---
+++ src/components/sections/QuoteOfTheDaySection/QuoteOfTheDaySection.tsx
@@ -0,0 +1,23 @@
+import styles from "./QuoteOfTheDaySection.module.scss";
+
+import { H1, P } from "@/components/ui/Typography";
+import { QuoteBlock } from "@/components/QuoteBlock";
+
+import quotes from "@/data/quotes.json";
+
+interface Props {}
+
+const QuoteOfTheDaySection: React.FC<Props> = () => {
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  return (
+    <div className={styles.section}>
+      <H1 transform="capitalize" className={styles.headline}>
+        Quote of the day
+      </H1>
+      <QuoteBlock quote={quote} />
+    </div>
+  );
+};
+
+export { QuoteOfTheDaySection };

bc2f190cd06592830692712a9fa6fe77c49d44d0
aissa-dev1 <aissadev1@gmail.com>
2025-05-29 19:55:49 +0100

    Install embla-carousel and create Carousel and FeaturedTale and FeaturedTalesSectionCarousel

---
+++ src/components/sections/FeaturedTalesSection/FeaturedTalesSectionCarousel.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+import styles from "./FeaturedTalesSection.module.scss";
+
+import { FeaturedTale, TaleType } from "@/components/Tale";
+import { Skeleton } from "@/components/Skeleton";
+
+const Carousel = dynamic(
+  () => import("@/components/ui/Carousel").then((mod) => mod.Carousel),
+  {
+    ssr: false,
+    loading: () => <Skeleton type="card" height={450} />,
+  }
+);
+
+interface Props {
+  tales: TaleType[];
+}
+
+const FeaturedTalesSectionCarousel: React.FC<Props> = ({ tales }) => {
+  return (
+    <Carousel>
+      {tales.map((tale) => (
+        <div key={tale._id} className={styles.carouselSlide}>
+          <FeaturedTale tale={tale} />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export { FeaturedTalesSectionCarousel };
